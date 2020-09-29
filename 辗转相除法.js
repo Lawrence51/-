173bd945ac45bae7f06d46790c1fb9f9f2ce3301
jie3.liu <jie3.liu@zhangmen.com>
2020-09-29 21:18:39 +0800

    feat: 更相减损术

--- 辗转相除法.js
+++ 辗转相除法.js
@@ -7,3 +7,13 @@ function getGreatestCommonDivisorV2(a,b) {
     }
     return getGreatestCommonDivisorV2(small, big%small)
 }
+
+// 更相减损术
+function deCome(a,b){
+    let big = a > b ? a:b;
+    let small = a < b ? a:b;
+    if (big - small == 0) {
+        return small
+    }
+    return deCome(big-small,small)
+}
