f605c185a5d90c3d4a707db6e536e41ae3b9c70b
jie3.liu <jie3.liu@zhangmen.com>
2020-10-10 16:12:20 +0800

    feat: 辗转相除法结合更相减损术

--- 辗转相除法.js
+++ 辗转相除法.js
@@ -24,6 +24,25 @@ function deCome(a,b){
 当a为偶数，b为奇数时，gcd(a,b) = gcd(a/2,b) = gcd(a>>1,b)。 
 当a为奇数，b为偶数时，gcd(a,b) = gcd(a,b/2) = gcd(a,b>>1)。 
 当a和b均为奇数时，先利用更相减损术运算一次，gcd(a,b) = gcd(b,a- b)，此时a-b必然是偶数，然后又可以继续进行移位运算。
+
+这是因为如果有一个为奇数一个为偶数的话，最大公约数肯定不会受2的影响，移位本身是要转化成2进制，然后再进行移位，
+这样的话其实就相当于除以2后的取整
 */
 
-// 这是因为如果有一个为奇数一个为偶数的话，最大公约数肯定不会受2的影响
+function gcd(a,b){
+    if (a - b == 0) {
+        return a
+    }
+    if ((a & 1) == 0 && (b & 1) == 0) {
+        return gcd(a>>1,b>>1)<<1; // 移位完成后记得移位回去，保证最大的公约数
+    } else if ((a & 1) == 0 && (b & 1) != 0){
+        return gcd(a>>1,b);
+    } else if ((a & 1) != 0 && (b & 1) == 0) {
+        return gcd(a,b>>1);
+    } else {
+        let big = a > b ? a : b;
+        let small = a < b ? a : b;
+        return gcd(big-small,small)
+    }
+}
+
