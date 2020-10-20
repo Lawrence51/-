124af7c6c9f622ba6969648586736d0e8f81d3df
jie3.liu <jie3.liu@zhangmen.com>
2020-10-20 20:11:03 +0800

    feat: 优化的一个方案

--- css/贪心算法.js
+++ css/贪心算法.js
@@ -30,3 +30,30 @@ function removeKDigits(num, k) {
     };
     return newNum;
 }
+
+function removeKDigits1(numStr, k) {
+    let numLength = numStr.length - k;
+    let stack = [];
+    let top = 0;
+    //遍历当前数字
+    for (let i = 0; i < numStr.length; i++) {
+        // 获取到当前遍历到的数字
+        let c = numStr.charAt(i);
+        /* 判断栈内的最后一个数字与当前数字的大小，
+        *  如果栈内最后一个数字大于当前遍历到的数字，那么就出栈边遍历到的数字入栈
+        */
+        while (top > 0 && stack[top-1] > c && k > 0) {
+            top -= 1;  // 如果满足上述条件，那么将top的值重置为上一次的值，然后重新复制 即可完成出栈入栈的交换
+            k -= 1;
+        }
+        stack[top++] = c;
+    }
+    // 把0去除
+    let str='';
+    for (let i = 0; i < stack.length; i++) {
+        if (stack[i] != 0) {
+            str += stack[i];
+        }
+    }
+    
+}
